8d440706b7f8f661575e238e86337cd019619b09
Rishi Sheth <rishi.sheth0@gmail.com>
2018-03-31 12:42:04 -0500

    updated get and added put; using repo and mapped to h2 via jpa

--- src/main/java/com/charter/enterprise/motd/MotdController.java
+++ src/main/java/com/charter/enterprise/motd/MotdController.java
@@ -1,16 +1,42 @@
 package com.charter.enterprise.motd;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.charter.enterprise.motd.repository.MotdRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RequestMapping("/")
 @RestController
 public class MotdController {
 
+    @Autowired
+    private MotdRepository motdRepository;
+
     @GetMapping
     public String getMotd() {
-        return "Welcome to Charter.  All systems are nominal.";
+        Optional<Motd> currentMotd = motdRepository.findById(1);
+        String motd = null;
+
+        if (currentMotd.isPresent())
+            motd = currentMotd.get().getMotd();
+
+        return motd;
     }
 
+    @PutMapping("/")
+    public ResponseEntity<Object> updateMotd(@RequestBody String motd) {
+        Optional<Motd> motdOptional = motdRepository.findById(1);
+
+        if (!motdOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        Motd updatedMotd = new Motd(1, motd);
+
+        motdRepository.save(updatedMotd);
+
+        return ResponseEntity.ok("Motd was updated!");
+    }
 }
