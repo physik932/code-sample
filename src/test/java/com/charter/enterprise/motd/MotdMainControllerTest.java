6a697f7997686f1f23faeb116fc721dcf484b0ff
Rishi Sheth <rishi.sheth0@gmail.com>
2018-03-31 13:40:20 -0500

    fix: use dirties context to reset state between tests
    - tests worked in specific order, which goes against isolated and
    stateless testing

--- src/test/java/com/charter/enterprise/motd/MotdMainControllerTest.java
+++ src/test/java/com/charter/enterprise/motd/MotdMainControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 public class MotdMainControllerTest {
     @Autowired
